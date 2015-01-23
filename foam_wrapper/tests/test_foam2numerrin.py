2c3860b9f8da3e19916a2d880d6ee66c8d3460e5
Kai Hiltunen <kai.hiltunen@numerola.fi>
2015-01-23 10:24:16 +0200

    Style changes

--- foam_wrapper/tests/test_foam2numerrin.py
+++ foam_wrapper/tests/test_foam2numerrin.py
@@ -7,11 +7,13 @@ mesh module functionalities
 
 import unittest
 from foam_wrapper.model import Model
-from foam_wrapper.foam_wrapper import Foam_wrapper
+from foam_wrapper.foam_wrapper import FoamWrapper
 from numerrin_wrapper.numerrin_wrapper import NumerrinWrapper
+import logging
+logger = logging.getLogger(__name__)
 
 class FoamNumerrinWrapperTestCase(unittest.TestCase):
-    """Test case for Foam_wrapper and NumerrinWrapper class"""
+    """Test case for FoamWapper and NumerrinWrapper class"""
     def setUp(self):
         """Creates dummy model to perform tests"""
         self.model = Model(1)
@@ -19,25 +21,25 @@ class FoamNumerrinWrapperTestCase(unittest.TestCase):
         
     def test_foamMesh2numerrinMesh(self):
         """Test mesh read from OpenFoam to SimPhony, pass to Numerrin to save to CGNS -format and back to SimPhony mesh"""
-        foam_wrapper = Foam_wrapper(self.model)
+        foam_wrapper = FoamWrapper(self.model)
         print "Read mesh from OpenFOAM"
         simphonyMesh = foam_wrapper.get_mesh("foam_wrapper/tests/FOAM_Meshes/pitzDaily")
-        print "Number of mesh points: ",sum(1 for _ in simphonyMesh.iter_points())
-        print "Number of mesh cells : ",sum(1 for _ in simphonyMesh.iter_cells())
-        print "Number of mesh faces : ",sum(1 for _ in simphonyMesh.iter_faces())
+        self.assertEqual(sum(1 for _ in simphonyMesh.iter_points()), 25012)
 
-        print "Mesh to Numerrin"
+        self.assertEqual(sum(1 for _ in simphonyMesh.iter_cells()), 12225)
+
+        self.assertEqual(sum(1 for _ in simphonyMesh.iter_faces()), 49180)
+
         numerrin_wrapper = NumerrinWrapper(self.model)
         numerrin_wrapper.importMesh(simphonyMesh)
-        print "Save to CGNS -format"
         numerrin_wrapper.setProgramString("WriteCGNS(\"mesh.cgns\") mesh\n")
         numerrin_wrapper.run()
-        print "Mesh from Numerrin"
         mesh2=numerrin_wrapper.exportMesh()
-        print type(mesh2).__name__
-        print "Number of mesh points: ",sum(1 for _ in mesh2.iter_points())
-        print "Number of mesh cells : ",sum(1 for _ in mesh2.iter_cells())
-        print "Number of mesh faces : ",sum(1 for _ in mesh2.iter_faces())
+        self.assertEqual(sum(1 for _ in mesh2.iter_points()), 25012)
+
+        self.assertEqual(sum(1 for _ in mesh2.iter_cells()), 12225)
+
+        self.assertEqual(sum(1 for _ in mesh2.iter_faces()), 49180)
         
 if __name__ == '__main__':
     unittest.main()
