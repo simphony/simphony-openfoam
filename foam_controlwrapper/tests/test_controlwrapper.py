05cd7d6dce1b15a494a1cc8ea70f53dc43e49be9
Kai Hiltunen <kai.hiltunen@numerola.fi>
2015-02-04 21:39:22 +0200

    BC's added

--- foam_controlwrapper/tests/test_controlwrapper.py
+++ foam_controlwrapper/tests/test_controlwrapper.py
@@ -18,7 +18,10 @@ class FoamControlWrapperTestCase(unittest.TestCase):
         pass
 
     def test_pitzDailyRun(self):
-        """Test to run pitzDaily example in OpenFoam"""
+        """Test to run pitzDaily example in OpenFoam"
+            -to get example working copy directory $FOAM_TUTORIALS/incompressible/simpleFoam/pitzDaily
+             to test -directory and run blockMesh on that directory
+        """
         foam_controlwrapper = FoamControlWrapper()
         foam_controlwrapper.CM[CUBA.NAME]="foam_controlwrapper/tests/pitzDaily"
 #        foam_controlwrapper.CM[CUBA.SOLVER]="simpleFoam"
@@ -26,6 +29,17 @@ class FoamControlWrapperTestCase(unittest.TestCase):
         foam_controlwrapper.SP[CUBA.NUMBEROF_TIME_STEPS]=500
         foam_controlwrapper.SP[CUBA.DENSITY] = 1.0
         foam_controlwrapper.SP[CUBA.DYNAMIC_VISCOSITY] = 1.0e-5
+# this is just an example. It is not enough for general setting of BC's
+        foam_controlwrapper.BC[CUBA.VELOCITY]={ 'inlet'        : (10, 0, 0), 
+                                                'outlet'       : 'zeroGradient', 
+                                                'upperWall'    : (0, 0, 0),
+                                                'lowerWall'    : (0, 0, 0),
+                                                'frontAndBack' : 'empty'}
+        foam_controlwrapper.BC[CUBA.PRESSURE]={ 'inlet'        : 'zeroGradient', 
+                                                'outlet'       : 0, 
+                                                'upperWall'    : 'zeroGradient',
+                                                'lowerWall'    : 'zeroGradient',
+                                                'frontAndBack' : 'empty'}
         
         foam_controlwrapper.run()
         
