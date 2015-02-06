7cb8f5c61874cc98d5b4f705fe42ec2eb8ec9bea
Kai Hiltunen <kai.hiltunen@numerola.fi>
2015-02-06 15:50:47 +0200

    changed installation to regular user

--- install_pyFoam.sh
+++ install_pyFoam.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+# install pyFoam as a regular user
 # fetch pyFoam
+cd ~
 svn co https://svn.code.sf.net/p/openfoam-extend/svn/trunk/Breeder/other/scripting/PyFoam/
 # move to installation directory
 cd PyFoam
-python setup.py install
-cd ..
+python setup.py install --user
+
