e23eddfd1ab236dc30dc6286ed43e02e8c3e12d2
Simphony <simphony@Simphony-test-bench.(none)>
2015-02-12 16:24:08 +0000

    fix import statement

--- foam_controlwrapper/__init__.py
+++ foam_controlwrapper/__init__.py
@@ -1,4 +1,4 @@
 # Functions, classes and constants exported here will be available
 # when the `openfoam` module is imported.
-from .code import FoamControlWrapper
+from .foam_controlwrapper import FoamControlWrapper
 __all__ = ['FoamControlWrapper']
