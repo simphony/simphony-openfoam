a982a812ea8363283733b76d86e7866a0df24769
enthought <enthought@simphony-base.(none)>
2015-03-03 19:16:16 +0000

    make sure that the openfoam enviroment has been activated

--- install_external.sh
+++ install_external.sh
@@ -8,4 +8,5 @@ svn co https://svn.code.sf.net/p/openfoam-extend/svn/trunk/Breeder/other/scripti
 cd PyFoam
 python setup.py install
 cd ..
+source /opt/openfoam222/etc/bashrc
 python check_PyFoam.py
