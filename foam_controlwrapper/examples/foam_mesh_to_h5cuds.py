123350cb0b0a26fc5a5bba4ec920016e058fc61c
Kai Hiltunen <kai.hiltunen@numerola.fi>
2015-03-11 18:31:20 +0200

    Mesh converter

---
+++ foam_controlwrapper/examples/foam_mesh_to_h5cuds.py
@@ -0,0 +1,16 @@
+from foam_controlwrapper.foam_controlwrapper import FoamControlWrapper
+from simphony.io.h5_cuds import H5CUDS
+
+foam_controlwrapper = FoamControlWrapper()
+name = 'poiseuille'
+path = '.'
+mesh_inside_wrapper = foam_controlwrapper.read_foammesh(name, path)
+
+
+mesh_file = H5CUDS.open("poiseuille.cuds")
+
+
+print 'Adding mesh ', mesh_inside_wrapper.name
+
+mesh_file.add_mesh(mesh_inside_wrapper)
+
