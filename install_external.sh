1421d53e7a0532587b5fbfae8ea9a60f2f154e38
enthought <enthought@simphony-base.(none)>
2015-03-12 12:10:16 +0000

    more cleanup

--- install_external.sh
+++ install_external.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-# install pyFoam 
+# install pyFoam
 # fetch pyFoam
 svn co https://svn.code.sf.net/p/openfoam-extend/svn/trunk/Breeder/other/scripting/PyFoam/
 # move to installation directory
@@ -9,7 +9,3 @@ python setup.py install
 cd ..
 source /opt/openfoam222/etc/bashrc
 python check_PyFoam.py
-# make and setup interface library
-cd openfoam-interface
-wmake libso
-python setup.py install
