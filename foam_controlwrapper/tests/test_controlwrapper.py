27e0aa46c89692ab190c58f41d880feb84d0b87f
Kai Hiltunen <kai.hiltunen@numerola.fi>
2015-03-31 13:16:43 +0300

    Parallel test added

--- foam_controlwrapper/tests/test_controlwrapper.py
+++ foam_controlwrapper/tests/test_controlwrapper.py
@@ -269,5 +269,44 @@ class FoamControlWrapperTestCase(unittest.TestCase):
 
         mesh_file.close()
 
+    def test_parallel_run(self):
+        """Test that field variable value is changed after
+        consecutive calls of run method
+
+        """
+
+        wrapper = FoamControlWrapper()
+        name = 'simplemesh'
+        wrapper.CM[CUBA.NAME] = name
+        wrapper.CM_extensions[CUBAExt.GE] = (CUBAExt.INCOMPRESSIBLE,
+                                             CUBAExt.LAMINAR_MODEL)
+        wrapper.CM_extensions[CUBAExt.NUMBER_OF_CORES] = 2
+        wrapper.SP[CUBA.TIME_STEP] = 1
+        wrapper.SP[CUBA.NUMBER_OF_TIME_STEPS] = 1
+        wrapper.SP[CUBA.DENSITY] = 1.0
+        wrapper.SP[CUBA.DYNAMIC_VISCOSITY] = 1.0
+        wrapper.BC[CUBA.VELOCITY] = {'boundary0': (0.1, 0, 0),
+                                     'boundary1': 'zeroGradient',
+                                     'boundary2': (0, 0, 0),
+                                     'boundary3': 'empty'}
+        wrapper.BC[CUBA.PRESSURE] = {'boundary0': 'zeroGradient',
+                                     'boundary1': 0,
+                                     'boundary2': 'zeroGradient',
+                                     'boundary3': 'empty'}
+        mesh_file = H5CUDS.open(os.path.join('foam_controlwrapper',
+                                             'tests',
+                                             'simplemesh.cuds'))
+        mesh_from_file = mesh_file.get_mesh(name)
+
+        mesh_inside_wrapper = wrapper.add_mesh(mesh_from_file)
+
+        wrapper.run()
+
+        if os.path.exists(mesh_inside_wrapper.path):
+            shutil.rmtree(mesh_inside_wrapper.path)
+
+        mesh_file.close()
+
+
 if __name__ == '__main__':
     unittest.main()
