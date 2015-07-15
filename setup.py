cb36e85de939969df947414cb062add0b7cd7cfe
Nathan Franklin <nathan.franklin@iwm.fraunhofer.de>
2015-07-15 14:46:54 +0200

    set to version 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.install import install
 with open('README.rst', 'r') as readme:
     README_TEXT = readme.read()
 
-VERSION = '0.1.3.dev0'
+VERSION = '0.1.3'
 
 
 class CleanCommand(Command):
