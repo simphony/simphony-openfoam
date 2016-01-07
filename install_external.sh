cf9767629943a142d9a924eafce18e34d120e8e2
Kai Hiltunen <kai.hiltunen@numerola.fi>
2016-01-07 12:18:21 +0200

    changed to openfoam230

--- install_external.sh
+++ install_external.sh
@@ -6,5 +6,5 @@ wget https://openfoamwiki.net/images/3/3b/PyFoam-0.6.4.tar.gz -O pyfoam_install/
 tar -xzf pyfoam_install/pyfoam.tgz -C pyfoam_install
 pip install --upgrade pyfoam_install/PyFoam-0.6.4
 rm -fr pyfoam_install
-source /opt/openfoam240/etc/bashrc
+source /opt/openfoam230/etc/bashrc
 python check_PyFoam.py
