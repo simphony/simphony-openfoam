fda212c89821e463ceda87f5b08f7c3c1a23482f
Kai Hiltunen <kai.hiltunen@numerola.fi>
2016-01-16 18:25:04 +0200

    time fix in internal wrapper

--- openfoam-interface/internal-interface/wrapper/libpythonfoam.cpp
+++ openfoam-interface/internal-interface/wrapper/libpythonfoam.cpp
@@ -1298,7 +1298,7 @@ static PyObject* setAllCellVectorData(PyObject *self, PyObject *args)
      double time;
      char *solver;
 
-     if (!PyArg_ParseTuple(args,"sisd",&name,&nproc,&solver,&initime)) {
+     if (!PyArg_ParseTuple(args,"sis",&name,&nproc,&solver)) {
       PyErr_SetString(PyExc_RuntimeError,"Invalid arguments");
       return NULL;
     }
