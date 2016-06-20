e1cd478275a6fd62952e8a8a69562639713799b9
Kai Hiltunen <kai.hiltunen@numerola.fi>
2016-06-20 15:00:03 +0300

    transportmodel added

---
+++ openfoam-interface/internal-interface/libs/incompressibleTransportModel/singlePhaseTransportModel/singlePhaseTransportModel.C
@@ -0,0 +1,97 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "singlePhaseTransportModel.H"
+#include "viscosityModel.H"
+#include "volFields.H"
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::singlePhaseTransportModel::singlePhaseTransportModel
+(
+    const volVectorField& U,
+    const surfaceScalarField& phi,
+    Istream& Is
+)
+:
+    IOdictionary
+    (
+        IOobject
+        (
+            "transportProperties",
+            U.time().constant(),
+            U.db(),
+            IOobject::MUST_READ_IF_MODIFIED,
+            IOobject::NO_WRITE
+	 ),
+	Is
+    ),
+    viscosityModelPtr_(viscosityModel::New("nu", *this, U, phi))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructors * * * * * * * * * * * * * * * //
+
+Foam::singlePhaseTransportModel::~singlePhaseTransportModel()
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField>
+Foam::singlePhaseTransportModel::nu() const
+{
+    return viscosityModelPtr_->nu();
+}
+
+
+Foam::tmp<Foam::scalarField>
+Foam::singlePhaseTransportModel::nu(const label patchi) const
+{
+    return viscosityModelPtr_->nu(patchi);
+}
+
+
+void Foam::singlePhaseTransportModel::correct()
+{
+    viscosityModelPtr_->correct();
+}
+
+
+bool Foam::singlePhaseTransportModel::read()
+{
+    if (regIOobject::read())
+    {
+        return viscosityModelPtr_->read(*this);
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+// ************************************************************************* //
