62c0dc0a568b16e00146ff52c2a800e439098e68
Stefano Borini <sborini@enthought.com>
2017-07-05 16:48:23 +0000

    Updated build

--- packageinfo.py
+++ packageinfo.py
@@ -5,4 +5,4 @@ VERSION = "0.2.4"
 
 # EDM build of the package. Increment in case of rebuilding an incorrectly
 # built package that has already been sent to the package server.
-BUILD = "2"
+BUILD = "5"
